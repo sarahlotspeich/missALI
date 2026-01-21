7896ab3984e51294c74e1a1f0f8342b9cbda194f
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-01-21 16:08:58 -0500

    Define vector of factor component names

--- R/miss_ind_approach.R
+++ R/miss_ind_approach.R
@@ -24,6 +24,10 @@ miss_ind_approach = function(outcome, covar = NULL, data, family) {
            BP_DIASTOLIC_F = make_miss_factor(x = BP_DIASTOLIC),
            BP_SYSTOLIC_F = make_miss_factor(x = BP_SYSTOLIC))
 
+  # Define vector of factor component names
+  factor_ALI_comp = c("A1C_F", "ALB_F", "BMI_F", "CHOL_F", "CRP_F",
+                      "CREAT_C_F", "HCST_F", "TRIG_F", "BP_DIASTOLIC_F","BP_SYSTOLIC_F")
+
   # Fit the model of interest
   if (!is.null(covar)) {
     fit_ind = glm(as.formula(paste(outcome, "~", paste(c(factor_ALI_comp, covar), collapse = "+"))),
