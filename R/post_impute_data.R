b919b99db18c5a1c08a6b0c370e557ac51932228
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-02-11 17:19:14 -0500

    Make post_impute_data work with "none"

--- R/post_impute_data.R
+++ R/post_impute_data.R
@@ -12,8 +12,18 @@ get_p = function(post_imputation, covar, ALI_comp_excl, ALI_comp) {
     p = length(covar) + 11 ### number of coefficients = covar + int + 10 comp
   }
 }
-
-post_impute_data = function(post_imputation, outcome, covar, data, family, use_glm) {
+## Initialize names with binary components
+ALI_comp = c("A1C", "ALB", "BMI", "CHOL", "CRP",
+             "CREAT_C", "HCST", "TRIG", "BP_DIASTOLIC", "BP_SYSTOLIC")
+# Essentially, all HCST would be treated as the same and CREAT_C is redundant for A1C
+## So, let's exclude them from our prediction model
+ALI_comp_excl = ALI_comp[-c(6, 7)] ## Remove the 6th and 7th element of ALI_comp
+post_impute_data = function(post_imputation, outcome, covar, data, family, components, use_glm) {
+  ### Replace with names of numeric components, if requested
+  if (imp_res$components == "numeric") {
+    ALI_comp = paste0("NUM_", ALI_comp)
+    ALI_comp_excl = paste0("NUM_", ALI_comp_excl)
+  }
   ### Post-imputation complete-case proportion approach
   if (post_imputation == "cc_prop") {
     #### Calculate complete-case proportion ALI from it
@@ -26,24 +36,44 @@ post_impute_data = function(post_imputation, outcome, covar, data, family, use_g
     #### Create missingness indicators for remaining, unimputed values
     imp_dat_b = num_miss_approach(outcome = outcome,
                                   covar = covar,
-                                  data = imp_dat_b,
+                                  data = data,
                                   family = family,
                                   use_glm = use_glm)
   } else if (post_imputation == "miss_ind") {
     #### Create missingness indicators for remaining, unimputed values
     imp_dat_b = miss_ind_approach(outcome = outcome,
                                   covar = covar,
-                                  data = imp_dat_b,
+                                  data = data,
                                   family = family,
                                   use_glm = use_glm)
   } else if (post_imputation %in% c("best", "worst")) {
     #### Replace unimputed values with best or worst case scenario
     imp_dat_b = case_approach(outcome = outcome,
                               covar = covar,
-                              data = imp_dat_b,
+                              data = data,
                               family = family,
                               best = post_imputation == "best",
                               use_glm = use_glm)
+  } else if (post_imputation == "none") {
+    if (use_glm) { ## Using a generalized linear model (GLM)
+      imp_dat_b = list(data = data,
+                       fit = glm(formula = as.formula(paste(outcome, "~", paste(c(ALI_comp_excl, covar), collapse = "+"))),
+                                 family = family,
+                                 data = data))
+    } else { ## Using a random forest
+      ### Fit random forest
+      if (family == "binomial") {
+        imp_dat_b = list(data = data,
+                         fit = ranger(
+                           formula = as.formula(paste(outcome, "~", paste(c(ALI_comp_excl, covar), collapse = "+"))),
+                           data = data,
+                           num.trees = 500,
+                           mtry = 2,
+                           importance = "permutation",
+                           probability = TRUE # For classification, to get class probabilities
+                         ))
+      }
+    }
   }
   return(imp_dat_b)
 }
