d41332d1140fc72f7d746bd2c38c585535b131c6
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-02-11 13:55:30 -0500

    Add RF option to case_approach and update documentation

--- R/case_approach.R
+++ R/case_approach.R
@@ -1,18 +1,19 @@
 #' Best/worst case scenario approach to fitting regression models with missing ALI components
-#' This function returns the dataset with missing values replaced by 0s (best case scenario) or 1s (worst case scenario) and the fitted model.
 #'
 #' @param outcome name of the outcome of the model (like \code{outcome = "disease"}).
 #' @param covar optional, vector of names for covariates of the model (like \code{covar = c("sex", "age")}). Default is \code{covar = NULL} (no additional covariates).
 #' @param data dataframe containing at least the variables included in \code{outcome}, \code{covar}, and the binary ALI components.
 #' @param family description of the error distribution and link function to be used in the model, to be passed to \code{glm()}.
 #' @param best if \code{TRUE} (the default), then all missing ALI components are replaced with \code{"healthy"}; otherwise, they are replaced with \code{"unhealthy"}.
+#' @param use_glm logical argument for whether a generalized linear model (GLM) should be used (\code{use_glm = TRUE}, the default). Otherwise, a random forest is used.
 #' @return
 #' \item{data}{dataframe with the factor versions of the ALI components (with missing values replaced by best/worst case scenario).}
 #' \item{fit}{fitted regression model object.}
 #' @export
 #' @importFrom dplyr mutate_at
 #' @importFrom tidyr replace_na
-case_approach = function(outcome, covar = NULL, data, family, best = TRUE) {
+#' @importFrom ranger ranger
+case_approach = function(outcome, covar = NULL, data, family, best = TRUE, use_glm = TRUE) {
   # Define vector of binary component names
   bin_ALI_comp = c("A1C", "ALB", "BMI", "CHOL", "CRP",
                    "CREAT_C", "HCST", "TRIG", "BP_DIASTOLIC", "BP_SYSTOLIC")
@@ -31,14 +32,23 @@ case_approach = function(outcome, covar = NULL, data, family, best = TRUE) {
   }
 
   # Fit the model of interest
-  if (!is.null(covar)) {
+  if (use_glm) { ## Using a generalized linear model (GLM)
     fit_case = glm(as.formula(paste(outcome, "~", paste(c(bin_ALI_comp, covar), collapse = "+"))),
                    family = family,
                    data = data)
-  } else {
-    fit_case = glm(as.formula(paste(outcome, "~", paste(bin_ALI_comp, collapse = "+"))),
-                   family = family,
-                   data = data)
+  } else { ## Using a random forest
+    if (family == "binomial") {
+      fit_case = ranger(
+        formula = as.formula(paste(outcome, "~", paste(c(bin_ALI_comp, covar), collapse = "+"))),
+        data = data,
+        num.trees = 500,
+        mtry = 2,
+        importance = "permutation",
+        probability = TRUE # For classification, to get class probabilities
+      )
+    } else {
+      fit_case = NULL
+    }
   }
 
   # Return list with the data and model
