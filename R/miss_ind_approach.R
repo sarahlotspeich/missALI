ab89ba27a747f77b5f0f2d354af6d1234aa282cb
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-04-20 10:23:54 -0400

    JK comment out the warning we just added

--- R/miss_ind_approach.R
+++ R/miss_ind_approach.R
@@ -36,8 +36,8 @@ miss_ind_approach = function(outcome, covar = NULL, data, family, use_glm = TRUE
     count_levels = apply(X = data[, factor_ALI_comp],
                          MARGIN = 2,
                          FUN = function(x) length(unique(x)))
-    warning(paste("The following variables were constant and excluded from the model:",
-                  paste(factor_ALI_comp[count_levels == 1], collapse = ", ")))
+    # warning(paste("The following variables were constant and excluded from the model:",
+    #               paste(factor_ALI_comp[count_levels == 1], collapse = ", ")))
     fit_ind = glm(as.formula(paste(outcome, "~", paste(c(factor_ALI_comp[count_levels > 1], covar), collapse = "+"))),
                   family = family,
                   data = data)
