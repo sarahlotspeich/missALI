763ff92c2017e9260fa0a95cd9386c13ceda6099
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-04-20 14:58:29 -0400

    Add ZIP to missingness indicator approach

--- R/miss_ind_approach.R
+++ R/miss_ind_approach.R
@@ -1,7 +1,8 @@
 #' Missingness indicator approach to fitting regression models with missing ALI components
 #'
 #' @param outcome name of the outcome of the model (like \code{outcome = "disease"}).
-#' @param covar optional, vector of names for covariates of the model (like \code{covar = c("sex", "age")}). Default is \code{covar = NULL} (no additional covariates).
+#' @param covar optional, vector of names for covariates of the outcome model (like \code{covar = c("sex", "age")}). Default is \code{covar = NULL} (no additional covariates).
+#' @param zeros optional, vector of names for covariates of the zero-inflation model (like \code{zeros = c("sex", "age")}). Default is \code{zeros = NULL} (no zero inflation). If zero inflation with only an intercept in the model is requested, use \code{zeros = "intercept"}.
 #' @param data dataframe containing at least the variables included in \code{outcome}, \code{covar}, and the binary ALI components.
 #' @param family description of the error distribution and link function to be used in the model, to be passed to \code{glm()}.
 #' @param use_glm logical argument for whether a generalized linear model (GLM) should be used (\code{use_glm = TRUE}, the default). Otherwise, a random forest is used.
@@ -11,7 +12,16 @@
 #' @export
 #' @importFrom dplyr mutate
 #' @importFrom ranger ranger
-miss_ind_approach = function(outcome, covar = NULL, data, family, use_glm = TRUE) {
+#' @importFrom pscl zeroinfl
+miss_ind_approach = function(outcome, covar = NULL, zeros = NULL, data, family, use_glm = TRUE) {
+  # Create indicator of whether zero-inflation is needed
+  use_zeroinfl = !is.null(zeroinfl)
+
+  ## If intercept only, overwrite zeros
+  if ("intercept" %in% zeros & length(zeros) == 1) {
+    zeros = c("1")
+  }
+
   # Create factor versions of ALI components with missingness indicators
   data = data |>
     mutate(A1C_F = make_miss_factor(x = A1C),
@@ -38,9 +48,15 @@ miss_ind_approach = function(outcome, covar = NULL, data, family, use_glm = TRUE
                          FUN = function(x) length(unique(x)))
     # warning(paste("The following variables were constant and excluded from the model:",
     #               paste(factor_ALI_comp[count_levels == 1], collapse = ", ")))
-    fit_ind = glm(as.formula(paste(outcome, "~", paste(c(factor_ALI_comp[count_levels > 1], covar), collapse = "+"))),
-                  family = family,
-                  data = data)
+    if (use_zeroinfl) {
+      fit_ind = zeroinfl(as.formula(paste(outcome, "~", paste(c(factor_ALI_comp[count_levels > 1], covar), collapse = "+"), "|", paste(zeros, collapse = "+"))),
+                         dist = family,
+                         data = data)
+    } else {
+      fit_ind = glm(as.formula(paste(outcome, "~", paste(c(factor_ALI_comp[count_levels > 1], covar), collapse = "+"))),
+                    family = family,
+                    data = data)
+    }
   } else { ## Using a random forest
     if (family == "binomial") {
       fit_ind = ranger(
